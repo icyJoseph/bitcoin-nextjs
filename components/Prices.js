bbe4ed21be9c082dd326c2c00c0ae61f249f4bf1
icyJoseph <sephxd1234@gmail.com>
2018-10-20 20:34:36 +0200

    feat: user can select favorite currency

--- components/Prices.js
+++ components/Prices.js
@@ -1,19 +1,47 @@
 // just to show that you can use React.Component directly!
 class Prices extends React.Component {
+  state = {
+    favorite: "EUR"
+  };
+
+  changeFavorite = e => this.setState({ favorite: e.target.value });
+
   render() {
     const { bpi } = this.props;
+    const { favorite } = this.state;
     const currencies = Object.keys(bpi);
+    const sortedCurrencies = currencies.includes(favorite)
+      ? [favorite].concat(currencies.filter(curr => curr !== favorite))
+      : currencies;
     return (
       <div>
         <ul className="list-group">
-          {currencies.map(currency => (
-            <li className="list-group-item">
+          {sortedCurrencies.map(currency => (
+            <li className="list-group-item" key={currency}>
               Bitcoin rate for {bpi[currency].description}:{" "}
-              <span class="badge bagde-primary">{bpi[currency].code}</span>
+              <span
+                className={`badge badge-${
+                  currency === favorite ? "danger" : "primary"
+                }`}
+              >
+                {bpi[currency].code}
+              </span>{" "}
               <strong>{bpi[currency].rate}</strong>
             </li>
           ))}
         </ul>
+        <br />
+        <select
+          value={favorite}
+          className="form-control"
+          onChange={this.changeFavorite}
+        >
+          {currencies.map(currency => (
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
+        </select>
       </div>
     );
   }
