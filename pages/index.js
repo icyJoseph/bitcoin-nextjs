779e6b92cd785e8023e529f574e4383cea54ae7e
icyJoseph <sephxd1234@gmail.com>
2018-10-20 20:03:44 +0200

    feat: fetch data from bitcoin api

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
+import axios from "axios";
 import Layout from "../components/Layout";
 
-const Index = () => (
+const Index = ({ bpi }) => (
   <Layout>
     <div>
       <h1>BitCoin Prices</h1>
+      <p>{bpi.USD.rate}</p>
     </div>
   </Layout>
 );
+
+Index.getInitialProps = async function() {
+  const { data } = await axios.get(
+    "https://api.coindesk.com/v1/bpi/currentprice.json"
+  );
+  return {
+    bpi: data.bpi
+  };
+};
 
 // As if JS was php, this index page gets loaded when the browser goes to the root of the url /
 export default Index;
