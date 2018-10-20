c1e169482ec6d4a400bcbaa5c0feee8a368a9ee4
icyJoseph <sephxd1234@gmail.com>
2018-10-20 12:25:02 +0200

    feat: / and /about pages

---
+++ pages/about.js
@@ -0,0 +1,2 @@
+// this one gets loaded in /about
+export default () => <div>This is the about page in /about</div>;
--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,2 @@
-export default () => <div>NextJS Index Page</div>
+// As if JS was php, this index page gets loaded when the browser goes to the root of the url /
+export default () => <div>NextJS Index Page</div>;
