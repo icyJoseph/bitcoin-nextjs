fb80c7827e8ba720d6b3bf9691b0bbba271ab511
icyJoseph <sephxd1234@gmail.com>
2018-10-20 13:29:00 +0200

    feat: add head tag with bootstrap

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,15 @@
+import Head from "next/head";
 import NavBar from "./NavBar";
 
 const Layout = props => (
   <div>
+    <Head>
+      <title>BitCoin Prices</title>
+      <link
+        rel="stylesheet"
+        href="https://bootswatch.com/4/cyborg/bootstrap.min.css"
+      />
+    </Head>
     <NavBar />
     {props.children}
   </div>
