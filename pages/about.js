9c7883d5c9116771ccc68918de780bcfb6d621cd
icyJoseph <sephxd1234@gmail.com>
2018-10-20 12:29:34 +0200

    feat: navigation between pages

--- pages/about.js
+++ pages/about.js
@@ -1,2 +1,23 @@
+import Link from "next/link";
+
+const About = () => (
+  <div>
+    <ul>
+      <li>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </li>
+    </ul>
+    <h1>About This Page</h1>
+    <p>You can see BitCoin prices in this page</p>
+  </div>
+);
+
 // this one gets loaded in /about
-export default () => <div>This is the about page in /about</div>;
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,22 @@
+import Link from "next/link";
+
+const Index = () => (
+  <div>
+    <ul>
+      <li>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </li>
+    </ul>
+    <h1>BitCoin Prices</h1>
+  </div>
+);
+
 // As if JS was php, this index page gets loaded when the browser goes to the root of the url /
-export default () => <div>NextJS Index Page</div>;
+export default Index;
